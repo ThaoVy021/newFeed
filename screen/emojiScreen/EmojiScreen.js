68b8082301bbad0321a15660e9d6f4d956f8969f
thaovy <thaovyluuthanh@gmail.com>
2022-04-18 19:21:30 +0700

    emoji

--- screen/emojiScreen/EmojiScreen.js
+++ screen/emojiScreen/EmojiScreen.js
@@ -4,47 +4,22 @@ import {render} from 'react-native/Libraries/Renderer/implementations/ReactNativ
 import {Component} from 'react/cjs/react.development';
 import {styles} from './style/emojiScreen.js/EmojiScreenStyle';
 
+const dataEmoji = [
+    require('../../asset/emoji/angry.png'),
+    require('../../asset/emoji/care.png'),
+    require('../../asset/emoji/haha.png'),
+    require('../../asset/emoji/love.png'),
+    require('../../asset/emoji/like.png'),
+    require('../../asset/emoji/sad.png'),
+];
+
 export default class Emoji extends Component {
   state = {
       imgFix: require('../../asset/emoji/love.png'),
-      imgOne: require('../../asset/emoji/angry.png'),
-      imgTwo: require('../../asset/emoji/care.png'),
-      imgThree: require('../../asset/emoji/haha.png'),
-      imgFour: require('../../asset/emoji/love.png'),
-      imgFive: require('../../asset/emoji/like.png'),
-      imgSix: require('../../asset/emoji/sad.png'),
   }
-
-  onChangeEmoji1 = () => {
-    {
-      this.setState({imgFix: this.state.imgOne})
-    }
-  };
-  onChangeEmoji2 = () => {
-    {
-      this.setState({imgFix: this.state.imgTwo})
-    }
-  };
-  onChangeEmoji3 = () => {
-    {
-      this.setState({imgFix: this.state.imgThree})
-    }
-  };
-  onChangeEmoji4 = () => {
-    {
-      this.setState({imgFix: this.state.imgFour})
-    }
-  };
-  onChangeEmoji5 = () => {
-    {
-      this.setState({imgFix: this.state.imgFive})
-    }
-  };
-  onChangeEmoji6 = () => {
-    {
-      this.setState({imgFix: this.state.imgSix})
-    }
-  };
+  onChangeEmoji = (index) => {
+    this.setState({imgFix: dataEmoji[index]})
+  }
 
   renderEmoji = () => {
     return (
@@ -61,42 +36,16 @@ export default class Emoji extends Component {
           />
         </View>
         <View style={styles.container_content_img_detail}>
-          <TouchableOpacity onPress={this.onChangeEmoji1.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgOne}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onChangeEmoji2.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgTwo}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onChangeEmoji3.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgThree}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onChangeEmoji4.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgFour}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onChangeEmoji5.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgFive}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.onChangeEmoji6.bind(this)}>
-            <Image
-              style={styles.container_content_img_detail_small}
-              source={this.state.imgSix}
-            />
-          </TouchableOpacity>
+        {
+          dataEmoji.map((data, index) => (
+            <TouchableOpacity key={index} onPress={() => this.onChangeEmoji(index)}>
+              <Image
+                style={styles.container_content_img_detail_small}
+                source={data}
+              />
+            </TouchableOpacity>
+          ))
+        }
         </View>
       </View>
     );
